57c4e7bcbf98e8a198ed117d07c6ed1a5d7faa4b
kno19005 <kno19005@byui.edu>
2020-01-31 01:15:58 -0700

    updated stuff

--- lesson4/assets/js/script.js
+++ lesson4/assets/js/script.js
@@ -6,7 +6,7 @@ const dayz = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Satur
 const monthz = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 
 let today = new Date();
-let day = today.getDay();
+let day = today.getDate();
 let dayName = dayz[today.getDay()];
 let month = monthz[today.getMonth()];
 let year = today.getFullYear();
@@ -16,3 +16,4 @@ let state = "Utah";
 let school = '<a href="https://www.byui.edu/online">BYUI Online Learning</a>';
 document.getElementById("footdiv").innerHTML = `&copy;The Elements | ${name} | ${state} | ${school}`;
 document.getElementById("lastUpdated").innerHTML = `${dayName}, ${day} ${month} ${year}`
+
