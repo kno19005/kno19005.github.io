b6299b66018eb62e16648ce1644c546b99c363a2
kno19005 <kno19005@byui.edu>
2020-01-30 19:47:45 -0700

    updated footer info

--- lesson4/assets/js/script.js
+++ lesson4/assets/js/script.js
@@ -2,10 +2,17 @@ function toggleMenu(){
     document.getElementsByClassName("navbar")[0].classList.toggle("magic");
 }
 
-var today = new Date();
-var year = "&copy;" + today.getFullYear();
+const dayz = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const monthz = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+
+let today = new Date();
+let day = today.getDay();
+let dayName = dayz[today.getDay()];
+let month = monthz[today.getMonth()];
+let year = today.getFullYear();
+
 let name = "Tommy Knorr";
 let state = "Utah";
 let school = '<a href="https://www.byui.edu/online">BYUI Online Learning</a>';
-document.getElementById("footdiv").innerHTML = `${year} | ${name} | ${state} | ${school}`;
-document.getElementById("lastUpdated").innerHTML = "Last Updated: " + document.lastModified;
+document.getElementById("footdiv").innerHTML = `&copy;The Elements | ${name} | ${state} | ${school}`;
+document.getElementById("lastUpdated").innerHTML = `${dayName}, ${day} ${month} ${year}`
