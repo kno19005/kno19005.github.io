7fdabefa6c8f32acd432ddf0af69a9d2ae61629b
kno19005 <kno19005@byui.edu>
2020-03-05 18:29:34 -0700

    updated lesson 9

--- lesson9/assets/js/towninfo.js
+++ lesson9/assets/js/towninfo.js
@@ -13,6 +13,7 @@ fetch(requestURL)
             // only work with town thats that exists in townset
             if (['Preston', 'Soda Springs', 'Fish Haven'].includes(town.name)) {
                 let card = document.createElement('section');
+                let childdiv = document.createElement('div'); // need this guy for switching image around
                 let h2 = document.createElement('h2');
                 let motto = document.createElement('p');
                 let founded = document.createElement('p');
@@ -20,12 +21,12 @@ fetch(requestURL)
                 let rainfall = document.createElement('p');
                 let image = document.createElement('img');
 
+                card.setAttribute('class', 'townsection');
                 h2.setAttribute('class','townname');
                 motto.setAttribute('class', 'townmotto');
                 founded.setAttribute('class', 'towndetails');
                 population.setAttribute('class', 'towndetails');
                 rainfall.setAttribute('class', 'towndetails');
-                rainfall.setAttribute('id', 'rainfall');
                 image.setAttribute('class', 'townimage');
 
                 h2.textContent = town.name;
@@ -37,11 +38,12 @@ fetch(requestURL)
                 image.setAttribute('src', "./assets/images/" + town.photo);            
                 image.setAttribute('alt', "picture of: " + h2.textContent);
 
-                card.appendChild(h2);
-                card.appendChild(motto);
-                card.appendChild(founded);
-                card.appendChild(population);
-                card.appendChild(rainfall);
+                childdiv.appendChild(h2);
+                childdiv.appendChild(motto);
+                childdiv.appendChild(founded);
+                childdiv.appendChild(population);
+                childdiv.appendChild(rainfall);
+                card.appendChild(childdiv);
                 card.appendChild(image);
 
                 document.querySelector('div.towninfo').appendChild(card);
