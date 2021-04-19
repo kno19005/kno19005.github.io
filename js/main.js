682a239069d87b740fdf5df97618146a64bc8333
Tom Knorr <kno19005@byui.edu>
2021-04-19 15:33:54 -0600

    Week 01

---
+++ js/main.js
@@ -0,0 +1,17 @@
+function loadListItems() {
+
+    const links = [{
+        label: "Week01",
+        url: "week01/index.html"
+    }];
+
+    document.getElementById("listItems").innerHTML = "";
+    for (const link in links) {
+        document.getElementById("listItems").innerHTML = `<li><a href=${links[link].url}>${links[link].label}</a></li>`
+    }
+}
+
+var today = new Date();
+let school = '<a href="https://www.byui.edu/online">BYUI Online Learning</a>';
+document.getElementById("footdiv").innerHTML = `&copy; ${today.getFullYear()} | Tommy Knorr`;
+document.getElementById("lastUpdated").innerHTML = "Last Updated: " + document.lastModified;
