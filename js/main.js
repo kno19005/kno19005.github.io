6b449cb51d87e7cf4be1732de34c1f54a422ae0b
Tom Knorr <kno19005@byui.edu>
2021-06-05 13:18:21 -0600

    Week 07

--- js/main.js
+++ js/main.js
@@ -1,34 +1,38 @@
 function loadListItems() {
 
     const links = [{
-        label: "Week 01",
-        url: "week01/index.html"
-    },
-    {
-        label: "Week 02",
-        url: "week02/index.html"
-    },
-    {
-        label: "Week 03",
-        url: "week03/index.html"
-    },
-    {
-        label: "Week 04",
-        url: "week04/index.html"
-    },
-    {
-        label: "Week 05",
-        url: "week05/index.html"
-    },
-    {
-        label: "Week 06 - ToDo",
-        url: "week06/index.html"
-    }
-];
+            label: "Week 01",
+            url: "week01/index.html"
+        },
+        {
+            label: "Week 02",
+            url: "week02/index.html"
+        },
+        {
+            label: "Week 03",
+            url: "week03/index.html"
+        },
+        {
+            label: "Week 04",
+            url: "week04/index.html"
+        },
+        {
+            label: "Week 05",
+            url: "week05/index.html"
+        },
+        {
+            label: "Week 06 - ToDo",
+            url: "week06/index.html"
+        },
+        {
+            label: "Week 07",
+            url: "week07/index.html"
+        }
+    ];
 
     document.getElementById("listItems").innerHTML = "";
     for (const link in links) {
-        document.getElementById("listItems").innerHTML += `<li><a href=${links[link].url}>${links[link].label}</a></li>`
+        document.getElementById("listItems").innerHTML += `<li class="splode"><a href=${links[link].url}>${links[link].label}</a></li>`
     }
 }
 
