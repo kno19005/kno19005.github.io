d03940c551b381d33dad7d593d2f710fe6f2a7be
Tom Knorr <kno19005@byui.edu>
2021-07-15 22:39:26 -0600

    Cleaned up code.

--- final/js/main.js
+++ final/js/main.js
@@ -1,11 +1,4 @@
-//import {loadCSS} from './utilities.js';
 import Stockology from "./stockology.js"
-
-// Load CSS/FontAwesome for icon display
-//loadCSS();
-
-// for debugging.. clear things out
-//localStorage.removeItem('todos');
 
 // Fire up thrusters.. pass in controls to object so object takes over
 const Stocks = new Stockology(
@@ -14,10 +7,3 @@ const Stocks = new Stockology(
     document.querySelector("#btnCompany"),
     document.querySelector("#msg"),
     document.querySelector("#resultTable"));
-
-// // Set Event for filter buttons
-// document.querySelectorAll(".task-filter").forEach(button => {
-//     button.addEventListener('click', () => {
-//         ToDoList.setFilter(button.value)
-//     });
-// });
